87a962c6e1502aee7c7546bae75286361bb5d059
Seth Schwartz <sschwartz214@gmail.com>
2018-12-18 20:13:14 -0700

    Sets up axios to return top urls json

--- src/components/TopUrlsContainer.js
+++ src/components/TopUrlsContainer.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 class TopUrlsContainer extends Component {
   constructor(props) {
@@ -8,7 +9,15 @@ class TopUrlsContainer extends Component {
     };
   }
 
-  componentDidMount() {}
+  componentDidMount() {
+    axios
+      .get("https://url-shortener--api.herokuapp.com/api/v1/top.json")
+      .then(response => {
+        console.log(response);
+        this.setState({ topUrls: response.data });
+      })
+      .catch(error => console.log(error));
+  }
 
   render() {
     return <div className="">Top 100 Urls</div>;
